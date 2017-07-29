42685065681a8b633dcc5d12ed19a962ffd50da8
Claudia Lu <claudialuclaudia@gmail.com>
2017-07-29 17:58:28 -0400

    ready for heroku (i think)

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost/test'
+	// 'url' : 'mongodb://localhost/test'
+	'url' : 'mongodb://heroku_r7mfmfz6:1i2jsafc0nm2d9uelq9urbju32@ds129023.mlab.com:29023/heroku_r7mfmfz6'
 }
